c94c59fc1950ff79e4fa10b88b1efcaedcea93c4
Andreas Aronsen Nesheim <andreas.nesheim@capgemini.com>
2022-05-07 18:51:54 +0200

    added dependency injection example

--- MauiBiometrics/MainPage.xaml.cs
+++ MauiBiometrics/MainPage.xaml.cs
@@ -5,15 +5,22 @@ namespace MauiBiometrics;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
+    private readonly IFingerprint fingerprint;
+
+    public MainPage(IFingerprint fingerprint)
 	{
 		InitializeComponent();
-	}
+        this.fingerprint = fingerprint;
+    }
 
 	private async void OnCounterClicked(object sender, EventArgs e)
 	{
 		var request = new AuthenticationRequestConfiguration("Prove you have fingers!", "Because without it you can't have access");
-		var result = await CrossFingerprint.Current.AuthenticateAsync(request);
+		// using DI
+		var result = await fingerprint.AuthenticateAsync(request);
+		// using static implementation
+		//var result = await CrossFingerprint.Current.AuthenticateAsync(request);
+
 		if (result.Authenticated)
 		{
 			await DisplayAlert("Authenticated!", "Access granted", "Cool beans");
--- MauiBiometrics/MauiProgram.cs
+++ MauiBiometrics/MauiProgram.cs
@@ -1,4 +1,7 @@
-﻿namespace MauiBiometrics;
+﻿using Plugin.Fingerprint;
+using Plugin.Fingerprint.Abstractions;
+
+namespace MauiBiometrics;
 
 public static class MauiProgram
 {
@@ -12,6 +15,9 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
+
+		builder.Services.AddSingleton<MainPage>();
+		builder.Services.AddSingleton(typeof(IFingerprint), CrossFingerprint.Current);
 
 		return builder.Build();
 	}
